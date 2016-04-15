2dd1f4649ef84d1dd00e16f246d359ee1b00a677
Christopher Paretti <cparetti@samsungaccelerator.com>
2016-04-15 18:33:44 -0400

    Rolling rolling checking

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+	public float speed;
+
+	private Rigidbody rb;
+
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = ne Maybe w Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup")) 
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+
+}
+	
